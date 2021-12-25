328e1f4d4d8d2c25389ec146f7f4bfbf87e41c77
swag <swaggboi@slackware.uk>
2021-12-25 01:46:39 -0500

    Clean up get_last_page()

--- lib/GuestbookNg/Model/Message.pm
+++ lib/GuestbookNg/Model/Message.pm
@@ -16,7 +16,7 @@ sub new($class, $pg, $pg_object) {
 sub get_posts($self, $this_page = undef) {
     if ($this_page) {
         my $row_count = $self->{'max_posts'};
-        my $offset = ($this_page - 1) * $row_count;
+        my $offset    = ($this_page - 1) * $row_count;
 
         $self->pg->db->query(<<~'END_SQL', $row_count, $offset)->arrays();
             SELECT to_char(message_date, 'Dy Mon DD HH:MI:SS AM TZ YYYY'),
@@ -50,15 +50,11 @@ sub max_posts($self, $value = undef) {
 }
 
 sub get_last_page($self) {
-    my $post_count = get_post_count($self);
+    my $post_count = $self->get_post_count();
+    my $last_page  = sprintf('%d', $post_count / $self->{'max_posts'});
 
     # Add a page if we have "remainder" posts
-    if ($post_count % $self->{'max_posts'}) {
-        sprintf('%d', $post_count / $self->{'max_posts'}) + 1
-    }
-    else {
-        sprintf('%d', $post_count / $self->{'max_posts'})
-    }
+    return $post_count % $self->{'max_posts'} ? ++$last_page : $last_page;
 }
 
 sub get_post_count($self) {
